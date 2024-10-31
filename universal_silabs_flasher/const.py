a0bfa8e1d87e71b5d56a44edc08a1dd3e546fa61
Tim Lunn <tl@smlight.tech>
2024-10-31 20:24:14 +1100

    add router types to const

--- universal_silabs_flasher/const.py
+++ universal_silabs_flasher/const.py
@@ -3,6 +3,7 @@ import enum
 
 class FirmwareImageType(enum.Enum):
     ZIGBEE_NCP = "zigbee_ncp"
+    ZIGBEE_ROUTER = "zigbee_router"
     OPENTHREAD_RCP = "openthread_rcp"
     ZWAVE_NCP = "zwave_ncp"
     BOOTLOADER = "bootloader"
@@ -21,11 +22,13 @@ LEGACY_FIRMWARE_TYPE_REMAPPING = {
 }
 
 
+
 class ApplicationType(enum.Enum):
     GECKO_BOOTLOADER = "bootloader"
     CPC = "cpc"
     EZSP = "ezsp"
     SPINEL = "spinel"
+    ROUTER = "router"
 
 
 FW_IMAGE_TYPE_TO_APPLICATION_TYPE = {
@@ -33,6 +36,7 @@ FW_IMAGE_TYPE_TO_APPLICATION_TYPE = {
     FirmwareImageType.MULTIPAN: ApplicationType.CPC,
     FirmwareImageType.OPENTHREAD_RCP: ApplicationType.SPINEL,
     FirmwareImageType.BOOTLOADER: ApplicationType.GECKO_BOOTLOADER,
+    FirmwareImageType.ZIGBEE_ROUTER: ApplicationType.ROUTER,
 }
 
 
@@ -41,6 +45,7 @@ DEFAULT_BAUDRATES = {
     ApplicationType.CPC: [460800, 115200, 230400],
     ApplicationType.EZSP: [115200],
     ApplicationType.SPINEL: [460800],
+    ApplicationType.ROUTER: [115200],
 }
 
 
